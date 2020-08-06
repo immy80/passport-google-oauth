63620ff6381b5e43aa8ab6599280a1c6ed986ddd
= <codenation@Codes-MacBook-Pro.local>
2020-08-06 15:14:52 +0100

    connecting ti the database

--- config/keys.js
+++ config/keys.js
@@ -2,4 +2,7 @@ module.exports = {
   googleClientID:
     '564748063695-f5sh36u8f9795d1jlmupm904udolou1h.apps.googleusercontent.com',
   googleClientSecret: '3KQNWMfZiXjg4fgBMDTlYTVg',
+  mongoURI:
+    ' mongodb+srv://imran:Blackcab123@cluster0-vxmxb.mongodb.net/google-oauth?retryWrites=true&w=majority',
+  cookieKey: 'redacted',
 };
